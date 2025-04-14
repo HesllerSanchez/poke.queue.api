7830cea4a38bde350c57b77a1e2b631af3c18ec7
uayebcaballero <uayeb.caballero@gmail.com>
2025-04-14 05:50:10 -0600

    endpoint to interact with function app

---
+++ controllers/PokeRequestController.py
@@ -0,0 +1,53 @@
+import json 
+import logging
+
+from fastapi import HTTPException
+from models.PokeRequest import PokemonRequest
+from utils.database import execute_query_json
+from utils.AQueue import AQueue
+
+# configurar el logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+async def select_pokemon_request( id: int ):
+    try:
+        query = "select * from pokequeue.requests where id = ?"
+        params = (id,)
+        result = await execute_query_json( query , params )
+        result_dict = json.loads(result)
+        return result_dict
+    except Exception as e:
+        logger.error( f"Error selecting report request {e}" )
+        raise HTTPException( status_code=500 , detail="Internal Server Error" )
+
+
+async def update_pokemon_request( pokemon_request: PokemonRequest) -> dict:
+    try:
+        query = " exec pokequeue.update_poke_request ?, ?, ? "
+        if not pokemon_request.url:
+            pokemon_request.url = "";
+
+        params = ( pokemon_request.id, pokemon_request.status, pokemon_request.url  )
+        result = await execute_query_json( query , params, True )
+        result_dict = json.loads(result)
+        return result_dict
+    except Exception as e:
+        logger.error( f"Error updating report request {e}" )
+        raise HTTPException( status_code=500 , detail="Internal Server Error" )
+
+
+async def insert_pokemon_request( pokemon_request: PokemonRequest) -> dict:
+    try:
+        query = " exec pokequeue.create_poke_request ? "
+        params = ( pokemon_request.pokemon_type,  )
+        result = await execute_query_json( query , params, True )
+        result_dict = json.loads(result)
+
+        await AQueue().insert_message_on_queue( result )
+
+        return result_dict
+    except Exception as e:
+        logger.error( f"Error inserting report reques {e}" )
+        raise HTTPException( status_code=500 , detail="Internal Server Error" )
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import uvicorn
 import json
 from fastapi import FastAPI
 from utils.database import execute_query_json
+from controllers.PokeRequestController import insert_pokemon_request, update_pokemon_request, select_pokemon_request
+from models.PokeRequest import PokemonRequest
 
 app = FastAPI()
 
@@ -12,10 +14,24 @@ async def root():
     result_dict = json.loads(result)
     return result_dict
 
-
 @app.get("/api/version")
 async def version():
-    return { "version":  "0.1.0" }
+    return { "version":  "0.2.0" }
+
+
+@app.get("/api/request/{id}")
+async def select_request(id: int):
+    return await select_pokemon_request(id)
+
+@app.post("/api/request")
+async def create_request(pokemon_request: PokemonRequest):
+    return await insert_pokemon_request( pokemon_request )
+
+@app.put("/api/request")
+async def update_request(pokemon_request: PokemonRequest):
+    return await update_pokemon_request( pokemon_request )
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
