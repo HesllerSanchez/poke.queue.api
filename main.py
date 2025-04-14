bab11e1188552efbe649cdb5649fa645289250fe
uayebcaballero <uayeb.caballero@gmail.com>
2025-04-14 09:19:22 -0600

    cores

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import uvicorn
 import json
 from fastapi import FastAPI
 from utils.database import execute_query_json
-from controllers.PokeRequestController import insert_pokemon_request, update_pokemon_request, select_pokemon_request
+from controllers.PokeRequestController import insert_pokemon_request, update_pokemon_request, select_pokemon_request, get_all_request
 from models.PokeRequest import PokemonRequest
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todos los orígenes
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 @app.get("/")
 async def root():
@@ -16,12 +24,16 @@ async def root():
 
 @app.get("/api/version")
 async def version():
-    return { "version":  "0.2.0" }
+    return { "version":  "0.3.1" }
 
 
 @app.get("/api/request/{id}")
 async def select_request(id: int):
     return await select_pokemon_request(id)
+
+@app.get("/api/request")
+async def select_all_request():
+    return await get_all_request()
 
 @app.post("/api/request")
 async def create_request(pokemon_request: PokemonRequest):
